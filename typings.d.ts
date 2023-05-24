83037b30b9ac8e99605156e1ece5e07a32dbeb92
Wayne P <wayne@iplayulisten.com>
2023-05-24 00:50:47 -0400

    adds: decl typings

--- typings.d.ts
+++ typings.d.ts
@@ -1,9 +1,39 @@
+interface StreamServiceConfig {
+  /**
+   * Socket.io instance
+   */
+  socket: object;
+  /**
+   * WebRTC ICE configuration
+   */
+  iceConfig: object;
+  /**
+   * StreamService logging
+   */
+  logging?: {
+    log: boolean;
+    warn: boolean;
+    error: boolean;
+  };
+}
+
 declare module "@waynecodez/wrtc-stream" {
   export default class StreamService {
-    constructor(props: any);
-
+    /**
+     * StreamService module abstracts WebRTC / Socket.io peer connections
+     */
+    constructor(config: StreamServiceConfig);
+    /**
+     * socket.io `on` event listener
+     */
     on: (arg0: string, arg2: any) => void;
+    /**
+     * socket.io `log` event emitter
+     */
     log: (arg0: string, arg2: any) => void;
+    /**
+     * socket.io `error` event listener
+     */
     error: (msg: string) => void;
 
     joinRoom: (name: string, room: string) => void;
