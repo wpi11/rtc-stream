3280ef54d53bb26fa20c8107319eb193b054c6dc
Wayne P <wayne@iplayulisten.com>
2023-05-24 13:30:56 -0400

    adds: server error handling

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-import chalk from 'chalk';
-import { start } from './server.js';
+import chalk from "chalk";
+import { start } from "./server.js";
 
 start()
-	.then((port) => console.log(chalk.green('rtc peer server listening on:', port)))
-	.catch((err) => console.log(chalk.red('failed to launch stream server.', err.message)));
+  .then((port) =>
+    console.log(chalk.green("rtc peer server listening on:", port))
+  )
+  .catch((err) =>
+    console.log(chalk.red("failed to launch stream server.", err.message))
+  );
