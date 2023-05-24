b2f32f755d637dbb112ff57c9c1da125d0331964
Wayne P <wayne@iplayulisten.com>
2023-05-24 13:30:18 -0400

    adds: server error handling

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-import chalk from "chalk";
-import { start } from "./server.js";
+import chalk from 'chalk';
+import { start } from './server.js';
 
 start()
-  .then((port) =>
-    console.log(chalk.green("rtc peer server listening on:", port))
-  )
-  .catch(() => console.log(chalk.red("failed to launch stream server.")));
+	.then((port) => console.log(chalk.green('rtc peer server listening on:', port)))
+	.catch((err) => console.log(chalk.red('failed to launch stream server.', err.message)));
--- server.js
+++ server.js
@@ -1,141 +1,140 @@
 /* eslint-disable no-unused-vars */
-import express from "express";
-import http from "http";
-import { Server } from "socket.io";
-import cors from "cors";
-import path from "path";
-import chalk from "chalk";
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+import cors from 'cors';
+import chalk from 'chalk';
+// import path from 'path';
 
 const PORT = 8080;
-const isDev = process.env.NODE_ENV === "development";
+const isDev = process.env.NODE_ENV === 'development';
 
 export const start = () =>
-  new Promise((resolve, reject) => {
-    const app = express();
-    const server = http.Server(app);
+	new Promise((resolve, reject) => {
+		try {
+			const app = express();
+			const server = http.Server(app);
 
-    const mode = isDev ? "development" : "production";
+			const mode = isDev ? 'development' : 'production';
 
-    const io = new Server(server);
+			const io = new Server(server);
 
-    let producer = null;
-    let producers = {};
-    let clients = {};
+			let producer = null;
+			let producers = {};
+			let clients = {};
 
-    io.on("connection", (socket) => {
-      console.log("websocket server connected:", { id: socket.id, mode });
+			io.on('connection', (socket) => {
+				console.log('websocket server connected:', { id: socket.id, mode });
 
-      function log() {
-        const arr = ["Server ->"];
-        arr.push.apply(arr, arguments);
-        socket.emit("log", arr);
-      }
+				function log() {
+					const arr = ['Server ->'];
+					arr.push.apply(arr, arguments);
+					socket.emit('log', arr);
+				}
 
-      socket.on("producer", (payload) => {
-        console.log("registered producer:", payload.name);
-        producer = socket.id;
-        socket.broadcast.emit("producer", { ...payload, id: socket.id });
-      });
+				socket.on('producer', (payload) => {
+					console.log('registered producer:', payload.name);
+					producer = socket.id;
+					socket.broadcast.emit('producer', { ...payload, id: socket.id });
+				});
 
-      socket.on("consumer", (payload) => {
-        console.log("requesting consumer:", payload);
-        socket.broadcast.emit("consumer", { ...payload, id: socket.id });
-      });
+				socket.on('consumer', (payload) => {
+					console.log('requesting consumer:', payload);
+					socket.broadcast.emit('consumer', { ...payload, id: socket.id });
+				});
 
-      socket.on("offer", (payload) => {
-        console.log("offer to:", payload.name, payload.id);
-        socket.broadcast
-          .to(payload.room)
-          .emit("offer", { ...payload, id: socket.id });
-      });
+				socket.on('offer', (payload) => {
+					console.log('offer to:', payload.name, payload.id);
+					socket.broadcast.to(payload.room).emit('offer', { ...payload, id: socket.id });
+				});
 
-      socket.on("answer", (payload) => {
-        console.log("answer to:", payload.name);
-        socket.to(payload.id).emit("answer", { ...payload, id: socket.id });
-      });
+				socket.on('answer', (payload) => {
+					console.log('answer to:', payload.name);
+					socket.to(payload.id).emit('answer', { ...payload, id: socket.id });
+				});
 
-      socket.on("candidate", (payload) => {
-        console.log("candidate to:", payload.name);
-        socket.broadcast
-          .to(payload.room)
-          .emit("candidate", { ...payload, id: socket.id });
-      });
+				socket.on('candidate', (payload) => {
+					console.log('candidate to:', payload.name);
+					socket.broadcast.to(payload.room).emit('candidate', { ...payload, id: socket.id });
+				});
 
-      // Host
-      socket.on("create or join", (event) => {
-        const name = event.name;
-        const room = event.room;
-        log("created / joined room:", { name, room });
+				// Host
+				socket.on('create or join', (event) => {
+					const name = event.name;
+					const room = event.room;
+					log('created / joined room:', { name, room });
 
-        // number of clients in the room
-        const clientsInRoom = io.sockets.adapter.rooms.get(room);
-        let numClients = clientsInRoom ? clientsInRoom.size : 0;
+					// number of clients in the room
+					const clientsInRoom = io.sockets.adapter.rooms.get(room);
+					let numClients = clientsInRoom ? clientsInRoom.size : 0;
 
-        if (numClients === 0) {
-          log(`Producer ${name} created room:`, { room, numClients });
-          producers[socket.id] = name;
-          socket.join(room);
-          socket.emit("created", { ...event, id: socket.id });
-        } else {
-          log(`Member ${name} joined room:`, { room, numClients });
-          clients[socket.id] = name;
+					if (numClients === 0) {
+						log(`Producer ${name} created room:`, { room, numClients });
+						producers[socket.id] = name;
+						socket.join(room);
+						socket.emit('created', { ...event, id: socket.id });
+					} else {
+						log(`Member ${name} joined room:`, { room, numClients });
+						clients[socket.id] = name;
 
-          io.sockets.in(room).emit("join", { ...event, id: socket.id });
-          socket.join(room);
-          io.to(socket.id).emit("joined", { ...event, id: socket.id });
-          io.sockets.in(room).emit("ready", { ...event, id: socket.id });
-        }
-      });
+						io.sockets.in(room).emit('join', { ...event, id: socket.id });
+						socket.join(room);
+						io.to(socket.id).emit('joined', { ...event, id: socket.id });
+						io.sockets.in(room).emit('ready', { ...event, id: socket.id });
+					}
+				});
 
-      socket.on("message", (event) => {
-        console.log("Client message:", socket.id, event.type);
-        const toId = event?.toId;
-        const room = event?.room;
+				socket.on('message', (event) => {
+					console.log('Client message:', socket.id, event.type);
+					const toId = event?.toId;
+					const room = event?.room;
 
-        if (toId) {
-          log("To Signal Message:", { socket: socket.id, event: event.type });
-          console.log("To Signal Message:", {
-            socket: socket.id,
-            event: event.type,
-          });
-          io.to(toId).emit("message", { ...event, id: socket.id });
-        } else if (room) {
-          log("Room Signal Message:", { socket: socket.id, event: event.type });
-          console.log("Room Signal Message:", {
-            socket: socket.id,
-            event: event.type,
-            room,
-          });
-          socket.broadcast
-            .to(room)
-            .emit("message", { ...event, id: socket.id });
-        } else {
-          log("Broadcast Signal:", { socket: socket.id, event: event.type });
-          console.log("Broadcast Signal Message:", {
-            socket: socket.id,
-            event: event.type,
-          });
-          socket.broadcast.emit("message", { ...event, id: socket.id });
-        }
-      });
+					if (toId) {
+						log('To Signal Message:', { socket: socket.id, event: event.type });
+						console.log('To Signal Message:', {
+							socket: socket.id,
+							event: event.type
+						});
+						io.to(toId).emit('message', { ...event, id: socket.id });
+					} else if (room) {
+						log('Room Signal Message:', { socket: socket.id, event: event.type });
+						console.log('Room Signal Message:', {
+							socket: socket.id,
+							event: event.type,
+							room
+						});
+						socket.broadcast.to(room).emit('message', { ...event, id: socket.id });
+					} else {
+						log('Broadcast Signal:', { socket: socket.id, event: event.type });
+						console.log('Broadcast Signal Message:', {
+							socket: socket.id,
+							event: event.type
+						});
+						socket.broadcast.emit('message', { ...event, id: socket.id });
+					}
+				});
 
-      socket.on("disconnect", () => {
-        delete producers[socket.id];
-        delete clients[socket.id];
-        socket.leave();
-      });
-    });
+				socket.on('disconnect', () => {
+					delete producers[socket.id];
+					delete clients[socket.id];
+					socket.leave();
+				});
+			});
 
-    app.use(cors());
+			app.use(cors());
 
-    app.use(express.static("build"));
+			// TODO: remove this since we're abstracting rtc server
+			// app.use(express.static("build"));
 
-    app.get("*", (req, res) => {
-      res.sendFile(path.resolve("build/index.html"));
-    });
+			// app.get("*", (req, res) => {
+			//   res.sendFile(path.resolve("build/index.html"));
+			// });
 
-    server.listen(PORT, () => {
-      console.log(chalk.yellow(`rtc peer server started..`));
-      resolve(PORT);
-    });
-  });
+			server.listen(PORT, () => {
+				console.log(chalk.yellow(`rtc peer server started..`));
+				resolve(PORT);
+			});
+		} catch (error) {
+			reject(error);
+		}
+	});
